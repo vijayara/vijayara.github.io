8400ffcb4b5c4e99e89daeec022939c0ce228bd6
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2020-04-05 14:41:09 +0200

    add the matching table

--- investor_profile.php
+++ investor_profile.php
@@ -1,6 +1,6 @@
 <div class= "row">
 	<div class="col-sm-12">	
-		<h1> Agile Profile </h1>
+		<h1> Agile Profile  - Batmaid</h1>
 	</div>
 </div>
 
@@ -9,6 +9,7 @@
 	<div class="col-sm-12">		
 
 <div class="table-responsive">
+	<h6>Matching</h6>
 
 <table>
 
@@ -21,9 +22,17 @@
 	<tr>
 	  <td>Cleaning public transport vehicules</td>
 	  <td>Transport public Lausanne</td>
-	  <td>05.04.2020</td>
-	  
+	  <td>05.04.2020</td>	  
 	</tr>	
+
+	<tr>
+	  <td>Clean and deinsfect the buildings hallways</td>
+	  <td>Regi SA</td>
+	  <td>10.04.2020</td>	  
+	</tr>	
+
+
+
 
 </table>
 
