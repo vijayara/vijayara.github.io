a1e43dbdf032f95f27f950f452798f2e652475ca
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2020-04-05 16:39:33 +0200

    add

--- agile_profile.php
+++ agile_profile.php
@@ -10,7 +10,7 @@
 </div>	
 
 <div class= "row">
-	<div class="col-sm-12">		
+	<div class="col-sm-2">		
 	</div>	
 <div class="col-sm-8">		
 <div class="table-responsive" style="padding:30px;">
---
+++ confirmation.php
@@ -0,0 +1,23 @@
+<div class= "row">
+	<div class="col-sm-8">
+		<h1> Confirmation</h1>
+	</div>	
+	<div class="col-sm-2">	
+		
+	</div>	
+	<div class="col-sm-2">
+	</div>	
+</div>	
+
+<div class= "row">
+	<div class="col-sm-2">		
+	</div>	
+<div class="col-sm-8">		
+
+	<h3>The confederation has grated the autorisation to let Barmaid continue there activities by filling the gaps with theire expetise.</h3>
+
+</div>
+		
+<div class="col-sm-2"></div>
+</div>
+
