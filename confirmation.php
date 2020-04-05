b71996dae0c7807f9d9cc75ddecbe16ef260fa7d
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2020-04-05 17:17:24 +0200

    aslkdja

--- confirmation.php
+++ confirmation.php
@@ -14,7 +14,7 @@
 	</div>	
 <div class="col-sm-8">		
 
-	<h3>The confederation has grated the autorisation to let Barmaid continue there activities by filling the gaps with theire expetise.</h3>
+	<h3>The confederation has grated the autorisation to let Barmaid continue there activities by filling the gaps with their expertise.</h3>
 
 </div>
 		
