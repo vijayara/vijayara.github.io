238471014eb0f9ffd4efbef6966f4fcfba35a1b3
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2020-04-05 14:35:27 +0200

    entreprise match list

--- investor_profile.php
+++ investor_profile.php
@@ -1,1 +1,33 @@
-<p>salut</p>
+<div class= "row">
+	<div class="col-sm-12">	
+		<h1> Agile Profile </h1>
+	</div>
+</div>
+
+
+<div class= "row">
+	<div class="col-sm-12">		
+
+<div class="table-responsive">
+
+<table>
+
+	<tr>
+	  <th>Description</th>
+	  <th>Institution</th>
+	  <th>Date</th>
+	  
+	</tr>
+	<tr>
+	  <td>Cleaning public transport vehicules</td>
+	  <td>Transport public Lausanne</td>
+	  <td>05.04.2020</td>
+	  
+	</tr>	
+
+</table>
+
+
+	</div>
+		</div>
+</div>
