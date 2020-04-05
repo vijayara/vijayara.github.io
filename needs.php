2296f5b79cd43369352b7b8eab4f8af19fb2f45d
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2020-04-05 16:43:09 +0200

    add chevron

--- needs.php
+++ needs.php
@@ -28,7 +28,7 @@
 	  <td>Cleaning public transport vehicules</td>
 	  <td>Transport public Lausanne</td>
 	  <td>05.04.2020</td>
-	  <td><a data-toggle="modal" data-target="#myModal"><i class="fa fa-chevron-right"></i> </a></td>	  
+	  <td><a data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-chevron-right"></i> </a></td>	  
 
 <!-- <button type="button" class="btn btn-info btn-lg" >Open Modal</button> -->
 
@@ -39,35 +39,35 @@
 	  <td>Clean and deinsfect the buildings hallways</td>
 	  <td>Regi SA</td>
 	  <td>10.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 	<tr>
 	  <td>Cook for hostpital</td>
 	  <td>CHUV</td>
 	  <td>14.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 	<tr>
 	  <td>Manage  calls in COVID-19 Information center</td>
 	  <td>Bern</td>
 	  <td>09.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 	<tr>
 	  <td>Produce hydroalcoholic gel for EMS</td>
 	  <td>EMS, Bienne</td>
 	  <td>09.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 	<tr>
 	  <td>Produce Medical Protective cloting</td>
 	  <td>CHUV</td>
 	  <td>09.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 
@@ -75,7 +75,7 @@
 	  <td>Produce Medical Protective cloting</td>
 	  <td>CHUV</td>
 	  <td>09.04.2020</td>
-	   <td><a data-toggle="modal" data-target="#myModal"> <i class="fa fa-chevron-right"></i></a></td>	  
+	   <td><a data-toggle="modal" data-target="#myModal"> <i class="glyphicon glyphicon-chevron-right"></i></a></td>	  
 	</tr>	
 
 
